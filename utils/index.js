955a86d8b09e65e2385ac61e348749a3aba4eead
Butcherok <Butcherok231@gmail.com>
2023-12-23 16:34:10 +0200

    fix createToken + hashPassword

--- utils/index.js
+++ utils/index.js
@@ -1,15 +1,15 @@
 const { tryCatchWrapper } = require("./tryCatchWrapper");
 const { httpError } = require("./httpError");
 const { schemaError } = require("./schemaError");
-// const { createToken } = require("./createToken");
-// const { hashPassword } = require("./hashPassword");
+const { createToken } = require("./createToken");
+const { hashPassword } = require("./hashPassword");
 const { Search } = require("./Search");
 
 module.exports = {
 	tryCatchWrapper,
 	httpError,
 	schemaError,
-	//   createToken,
-	//   hashPassword,
+	createToken,
+	hashPassword,
 	Search,
 };
