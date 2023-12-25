98ddfaa4913105027a2cd71dd44da7abf0a0f27b
Butcherok <Butcherok231@gmail.com>
2023-12-25 14:33:25 +0200

    fix current user method

--- models/index.js
+++ models/index.js
@@ -2,10 +2,10 @@ const { User } = require("./user/userModel");
 
 const { Notice } = require("./notice/noticeModel");
 
-// const { Pet } = require("./pet/petModel");
+const { Pet } = require("./pet/petModel");
 
 // const { Sponsor } = require("./sponsor/sponsorModel");
 
 // const { Article } = require("./article/articleModel");
 
-module.exports = { Notice, User };
+module.exports = { Notice, User, Pet };
