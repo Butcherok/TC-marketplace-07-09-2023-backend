704f86bf35866d131bc3c3dab91f39fb30ce837f
Butcherok <Butcherok231@gmail.com>
2024-04-13 13:57:07 +0300

    fix routes for register users

--- controllers/users/usersController.js
+++ controllers/users/usersController.js
@@ -2,199 +2,204 @@ const bcrypt = require("bcrypt");
 
 const { Users } = require("../../services");
 const {
-  tryCatchWrapper,
-  httpError,
-  createToken,
-  hashPassword,
+	tryCatchWrapper,
+	httpError,
+	createToken,
+	hashPassword,
 } = require("../../utils");
 const { errorMessage } = require("../../constants");
 
 const googleAuth = (req, res) => {
-  const url = `${process.env.FRONTEND_URL}?token=${req.user.token}&tokenLifeTime=${req.user.tokenLifeTime}&refreshToken=${req.user.refreshToken}`;
+	const url = `${process.env.FRONTEND_URL}?token=${req.user.token}&tokenLifeTime=${req.user.tokenLifeTime}&refreshToken=${req.user.refreshToken}`;
 
-  res.redirect(url);
+	res.redirect(url);
 };
 
 const register = async (req, res) => {
-  const { email, password, name } = req.body;
+	const { email, password, firstName, lastName } = req.body;
 
-  const user = await Users.findUserByQuery({ email });
-  if (user) throw httpError(409, errorMessage[409]);
+	const user = await Users.findUserByQuery({ email });
+	if (user) throw httpError(409, errorMessage[409]);
 
-  const hashPass = await hashPassword(password);
-  const [token, tokenLifeTime] = createToken(
-    { email },
-    process.env.JWT_KEY,
-    process.env.TOKEN_LIFE
-  );
-  const [refreshToken] = createToken(
-    { email },
-    process.env.REFRESH_JWT_KEY,
-    process.env.REFRESH_TOKEN_LIFE
-  );
+	const hashPass = await hashPassword(password);
+	const [token, tokenLifeTime] = createToken(
+		{ email },
+		process.env.JWT_KEY,
+		process.env.TOKEN_LIFE
+	);
+	const [refreshToken] = createToken(
+		{ email },
+		process.env.REFRESH_JWT_KEY,
+		process.env.REFRESH_TOKEN_LIFE
+	);
 
-  const newUser = await Users.createUser({
-    email,
-    password: hashPass,
-    token,
-    tokenLifeTime,
-    name,
-    refreshToken,
-  });
+	const newUser = await Users.createUser({
+		email,
+		password: hashPass,
+		token,
+		tokenLifeTime,
+		firstName,
+		lastName,
+		refreshToken,
+	});
 
-  res.json({
-    token,
-    tokenLifeTime,
-    refreshToken,
-    user: {
-      uid: newUser.id,
-      name: newUser.name,
-      email: newUser.email,
-      avatarUrl: newUser.avatarUrl,
-      isNewUser: true,
-    },
-  });
+	res.json({
+		token,
+		tokenLifeTime,
+		refreshToken,
+		user: {
+			uid: newUser.id,
+			firstName: newUser.firstName,
+			lastName: newUser.lastName,
+			email: newUser.email,
+			avatarUrl: newUser.avatarUrl,
+			isNewUser: true,
+		},
+	});
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  const user = await Users.findUserByQuery({ email });
+	const user = await Users.findUserByQuery({ email });
 
-  if (!user) throw httpError(401, errorMessage[401].wrongLogin);
+	if (!user) throw httpError(401, errorMessage[401].wrongLogin);
 
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-  if (!isPasswordValid) throw httpError(401, errorMessage[401].wrongLogin);
+	const isPasswordValid = await bcrypt.compare(password, user.password);
+	if (!isPasswordValid) throw httpError(401, errorMessage[401].wrongLogin);
 
-  const [token, tokenLifeTime] = createToken(
-    { id: user.id },
-    process.env.JWT_KEY,
-    process.env.TOKEN_LIFE
-  );
-  const [refreshToken] = createToken(
-    { id: user.id },
-    process.env.REFRESH_JWT_KEY,
-    process.env.REFRESH_TOKEN_LIFE
-  );
+	const [token, tokenLifeTime] = createToken(
+		{ id: user.id },
+		process.env.JWT_KEY,
+		process.env.TOKEN_LIFE
+	);
+	const [refreshToken] = createToken(
+		{ id: user.id },
+		process.env.REFRESH_JWT_KEY,
+		process.env.REFRESH_TOKEN_LIFE
+	);
 
-  const updatedUser = await Users.updateUser({
-    id: user.id,
-    data: { token, tokenLifeTime, refreshToken },
-    projection: "-password -avatarId",
-  }).populate("pets");
+	const updatedUser = await Users.updateUser({
+		id: user.id,
+		data: { token, tokenLifeTime, refreshToken },
+		projection: "-password -avatarId",
+	}).populate("goods");
 
-  res.json({
-    token: updatedUser.token,
-    tokenLifeTime: updatedUser.tokenLifeTime,
-    refreshToken: updatedUser.refreshToken,
-    user: {
-      uid: updatedUser.id,
-      email: updatedUser.email,
-      name: updatedUser.name,
-      birthday: updatedUser.birthday,
-      phone: updatedUser.phone,
-      city: updatedUser.city,
-      favorites: updatedUser.favorites,
-      pets: updatedUser.pets,
-      avatarUrl: updatedUser.avatarUrl,
-      isNewUser: false,
-    },
-  });
+	res.json({
+		token: updatedUser.token,
+		tokenLifeTime: updatedUser.tokenLifeTime,
+		refreshToken: updatedUser.refreshToken,
+		user: {
+			uid: updatedUser.id,
+			email: updatedUser.email,
+			firstName: updatedUser.firstName,
+			lastName: updatedUser.lastName,
+			birthday: updatedUser.birthday,
+			phone: updatedUser.phone,
+			city: updatedUser.city,
+			favorites: updatedUser.favorites,
+			pets: updatedUser.pets,
+			avatarUrl: updatedUser.avatarUrl,
+			isNewUser: false,
+		},
+	});
 };
 
 const refresh = async (req, res) => {
-  const [token, tokenLifeTime] = createToken(
-    { id: req.user.id },
-    process.env.JWT_KEY,
-    process.env.TOKEN_LIFE
-  );
-  const [refreshToken] = createToken(
-    { id: req.user.id },
-    process.env.REFRESH_JWT_KEY,
-    process.env.REFRESH_TOKEN_LIFE
-  );
-  await Users.updateUser({
-    id: req.user.id,
-    data: { token, tokenLifeTime, refreshToken },
-  });
+	const [token, tokenLifeTime] = createToken(
+		{ id: req.user.id },
+		process.env.JWT_KEY,
+		process.env.TOKEN_LIFE
+	);
+	const [refreshToken] = createToken(
+		{ id: req.user.id },
+		process.env.REFRESH_JWT_KEY,
+		process.env.REFRESH_TOKEN_LIFE
+	);
+	await Users.updateUser({
+		id: req.user.id,
+		data: { token, tokenLifeTime, refreshToken },
+	});
 
-  res.json({ token, tokenLifeTime, refreshToken });
+	res.json({ token, tokenLifeTime, refreshToken });
 };
 
 const logout = async (req, res) => {
-  const { id } = req.user;
+	const { id } = req.user;
 
-  await Users.updateUser({
-    id,
-    data: { token: "", refreshToken: "", tokenLifeTime: null },
-  });
+	await Users.updateUser({
+		id,
+		data: { token: "", refreshToken: "", tokenLifeTime: null },
+	});
 
-  res.sendStatus(204);
+	res.sendStatus(204);
 };
 
 const update = async (req, res) => {
-  const { id } = req.user;
-  const { body } = req;
+	const { id } = req.user;
+	const { body } = req;
 
-  const updatedUser = await Users.updateUser({
-    id,
-    data: body,
-    projection: "-password -avatarId",
-  }).populate("pets");
+	const updatedUser = await Users.updateUser({
+		id,
+		data: body,
+		projection: "-password -avatarId",
+	}).populate("pets");
 
-  res.json({
-    token: body.token ? body.token : updatedUser.token,
-    tokenLifeTime: body.tokenLifeTime
-      ? body.tokenLifeTime
-      : updatedUser.tokenLifeTime,
-    refreshToken: body.refreshToken
-      ? body.refreshToken
-      : updatedUser.refreshToken,
-    user: {
-      uid: updatedUser.id,
-      email: updatedUser.email,
-      name: updatedUser.name,
-      birthday: updatedUser.birthday,
-      phone: updatedUser.phone,
-      city: updatedUser.city,
-      favorites: updatedUser.favorites,
-      pets: updatedUser.pets,
-      avatarUrl: updatedUser.avatarUrl,
-      isNewUser: false,
-    },
-  });
+	res.json({
+		token: body.token ? body.token : updatedUser.token,
+		tokenLifeTime: body.tokenLifeTime
+			? body.tokenLifeTime
+			: updatedUser.tokenLifeTime,
+		refreshToken: body.refreshToken
+			? body.refreshToken
+			: updatedUser.refreshToken,
+		user: {
+			uid: updatedUser.id,
+			email: updatedUser.email,
+			firstName: updatedUser.firstName,
+			lastName: updatedUser.lastName,
+			birthday: updatedUser.birthday,
+			phone: updatedUser.phone,
+			city: updatedUser.city,
+			favorites: updatedUser.favorites,
+			pets: updatedUser.pets,
+			avatarUrl: updatedUser.avatarUrl,
+			isNewUser: false,
+		},
+	});
 };
 
 const getMe = async (req, res) => {
-  const { id } = req.user;
+	const { id } = req.user;
 
-  const user = await Users.findUserById(
-    id,
-    "-password -token -avatarId"
-  ).populate("pets");
+	const user = await Users.findUserById(
+		id,
+		"-password -token -avatarId"
+	).populate("pets");
 
-  res.json({
-    user: {
-      uid: user.id,
-      email: user.email,
-      name: user.name,
-      birthday: user.birthday,
-      phone: user.phone,
-      city: user.city,
-      pets: user.pets,
-      favorites: user.favorites,
-      avatarUrl: user.avatarUrl,
-      isNewUser: false,
-    },
-  });
+	res.json({
+		user: {
+			uid: user.id,
+			email: user.email,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			birthday: user.birthday,
+			phone: user.phone,
+			city: user.city,
+			goods: user.goods,
+			favorites: user.favorites,
+			avatarUrl: user.avatarUrl,
+			isNewUser: false,
+		},
+	});
 };
 
 module.exports = {
-  googleAuth: tryCatchWrapper(googleAuth),
-  register: tryCatchWrapper(register),
-  login: tryCatchWrapper(login),
-  refresh: tryCatchWrapper(refresh),
-  logout: tryCatchWrapper(logout),
-  update: tryCatchWrapper(update),
-  getMe: tryCatchWrapper(getMe),
+	googleAuth: tryCatchWrapper(googleAuth),
+	register: tryCatchWrapper(register),
+	login: tryCatchWrapper(login),
+	refresh: tryCatchWrapper(refresh),
+	logout: tryCatchWrapper(logout),
+	update: tryCatchWrapper(update),
+	getMe: tryCatchWrapper(getMe),
 };
--- models/user/userModel.js
+++ models/user/userModel.js
@@ -2,59 +2,64 @@ const { Schema, model } = require("mongoose");
 
 const { schemaError } = require("../../utils");
 const {
-  emailRegex,
-  phoneRegex,
-  cityRegex,
-  defaultAvatarUrl,
+	emailRegex,
+	phoneRegex,
+	cityRegex,
+	defaultAvatarUrl,
 } = require("../../constants");
 
 const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      match: emailRegex,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: function () {
-        this.googleId ? false : true;
-      },
-      default: "",
-    },
-
-    name: {
-      type: String,
-      minlength: 2,
-      maxlength: 15,
-      required: [true, "Set name for user"],
-    },
-    pets: [{ type: Schema.Types.ObjectId, ref: "pet" }],
-    favorites: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "notice",
-      },
-    ],
-    birthday: {
-      type: Date,
-      min: "1940-01-01",
-      max: new Date(),
-    },
-    phone: { type: String, match: phoneRegex, minlength: 13 },
-    city: { type: String, match: cityRegex, minlength: 2, maxlength: 30 },
-    avatarUrl: { type: String, default: defaultAvatarUrl },
-    avatarId: { type: String, default: "" },
-    googleId: { type: String, default: "" },
-    token: {
-      type: String,
-      default: "",
-    },
-    tokenLifeTime: { type: Date },
-    refreshToken: { type: String, default: "" },
-  },
-  { versionKey: false, timestamps: false }
+	{
+		email: {
+			type: String,
+			required: [true, "Email is required"],
+			match: emailRegex,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: function () {
+				this.googleId ? false : true;
+			},
+			default: "",
+		},
+		firstName: {
+			type: String,
+			minlength: 2,
+			maxlength: 15,
+			required: [true, "Set first name for user"],
+		},
+		lastName: {
+			type: String,
+			minlength: 2,
+			maxlength: 15,
+			required: [true, "Set  last name for user"],
+		},
+		goods: [{ type: Schema.Types.ObjectId, ref: "goods" }],
+		favorites: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "notice",
+			},
+		],
+		birthday: {
+			type: Date,
+			min: "1940-01-01",
+			max: new Date(),
+		},
+		phone: { type: String, match: phoneRegex, minlength: 13 },
+		city: { type: String, match: cityRegex, minlength: 2, maxlength: 30 },
+		avatarUrl: { type: String, default: defaultAvatarUrl },
+		avatarId: { type: String, default: "" },
+		googleId: { type: String, default: "" },
+		token: {
+			type: String,
+			default: "",
+		},
+		tokenLifeTime: { type: Date },
+		refreshToken: { type: String, default: "" },
+	},
+	{ versionKey: false, timestamps: true }
 );
 
 userSchema.post("save", schemaError);
